c59f52db6d329c95c14d42508e7ab308b4b819e3
sintjuri <sintjuri@yandex.ru>
2014-09-24 22:04:54 +0400

    authorization fix

--- app/src/main/java/com/onettm/publics/MainActivity.java
+++ app/src/main/java/com/onettm/publics/MainActivity.java
@@ -1,24 +1,13 @@
 package com.onettm.publics;
 
-import java.util.Locale;
-
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKScope;
@@ -27,71 +16,62 @@ import com.vk.sdk.VKSdkListener;
 import com.vk.sdk.VKUIHelper;
 import com.vk.sdk.api.VKError;
 import com.vk.sdk.dialogs.VKCaptchaDialog;
+import com.vk.sdk.util.VKUtil;
 
-
-public class MainActivity extends ActionBarActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
+/**
+ * Activity which displays a login screen to the user, offering registration as
+ * well.
+ */
+public class MainActivity extends FragmentActivity {
 
     /**
-     * The {@link ViewPager} that will host the section contents.
+     * Scope is set of required permissions for your application
+     * @see <a href="https://vk.com/dev/permissions">vk.com api permissions documentation</a>
      */
-    ViewPager mViewPager;
-
-    private static String[] sMyScope = new String[]{VKScope.FRIENDS, VKScope.WALL, VKScope.PHOTOS, VKScope.NOHTTPS, VKScope.PAGES};
-    private static String sTokenKey = "VK_ACCESS_TOKEN";
-    private static String APP_ID = "4551458";
+    private static final String[] sMyScope = new String[] {
+            VKScope.FRIENDS,
+            VKScope.WALL,
+            VKScope.PHOTOS,
+            VKScope.NOHTTPS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        VKSdk.initialize(sdkListener, APP_ID, VKAccessToken.tokenFromSharedPreferences(this, sTokenKey));
-        setContentView(R.layout.main);
-
-
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        VKSdk.authorize(sMyScope, true, false);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        setContentView(R.layout.activity_start);
+        VKUIHelper.onCreate(this);
+        VKSdk.initialize(sdkListener, "4551458");
+        if (VKSdk.wakeUpSession()) {
+            startTestActivity();
+            return;
         }
-        return super.onOptionsItemSelected(item);
+
+        String[] fingerprint = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        Log.d("Fingerprint", fingerprint[0]);
+    }
+    private void showLogout() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new LogoutFragment())
+                .commit();
+    }
+    private void showLogin() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new LoginFragment())
+                .commit();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         VKUIHelper.onResume(this);
+        if (VKSdk.isLoggedIn()) {
+            showLogout();
+        } else {
+            showLogin();
+        }
+
     }
 
     @Override
@@ -102,11 +82,10 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        VKUIHelper.onActivityResult(requestCode, resultCode, data);
+        VKUIHelper.onActivityResult(this, requestCode, resultCode, data);
     }
 
-    private VKSdkListener sdkListener = new VKSdkListener() {
+    private final VKSdkListener sdkListener = new VKSdkListener() {
         @Override
         public void onCaptchaError(VKError captchaError) {
             new VKCaptchaDialog(captchaError).show();
@@ -118,98 +97,81 @@ public class MainActivity extends ActionBarActivity {
         }
 
         @Override
-        public void onAccessDenied(VKError authorizationError) {
-            new AlertDialog.Builder(MainActivity.this)
-                    .setMessage(authorizationError.errorMessage)
+        public void onAccessDenied(final VKError authorizationError) {
+            new AlertDialog.Builder(VKUIHelper.getTopActivity())
+                    .setMessage(authorizationError.toString())
                     .show();
         }
 
         @Override
         public void onReceiveNewToken(VKAccessToken newToken) {
-            newToken.saveTokenToSharedPreferences(MainActivity.this, sTokenKey);
-            // Intent i = new Intent(LoginActivity.this, MainActivity.class);
-            // startActivity(i);
+            startTestActivity();
         }
 
         @Override
         public void onAcceptUserToken(VKAccessToken token) {
-            //Intent i = new Intent(LoginActivity.this, MainActivity.class);
-            // startActivity(i);
+            startTestActivity();
         }
     };
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
-
-
+    private void startTestActivity() {
+	    startActivity(new Intent(this, TestActivity.class));
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
+    public static class LoginFragment extends android.support.v4.app.Fragment {
+	    public LoginFragment() {
+		    super();
+	    }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_login, container, false);
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            getView().findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    VKSdk.authorize(sMyScope, true, false);
+                }
+            });
+
+	        getView().findViewById(R.id.force_oauth_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    VKSdk.authorize(sMyScope, true, true);
+                }
+            });
+        }
+    }
+	public static class LogoutFragment extends android.support.v4.app.Fragment {
+	    public LogoutFragment() {
+		    super();
+	    }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_logout, container, false);
         }
 
-    }
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+	        getView().findViewById(R.id.continue_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+	                ((MainActivity)getActivity()).startTestActivity();
+                }
+            });
 
+	        getView().findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    VKSdk.logout();
+                    if (!VKSdk.isLoggedIn()) {
+	                    ((MainActivity)getActivity()).showLogin();
+                    }
+                }
+            });
+        }
+    }
 }
